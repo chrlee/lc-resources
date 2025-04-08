c2697069ebb559620f68d7692b80a30794c493fb
chrlee <christianjslee@gmail.com>
2025-04-08 13:04:23 -0400

    Solve: binary-search. Binary Search (java)

---
+++ leetcode/binary-search_binary_search_1744131863414.java
@@ -0,0 +1,9 @@
+greater than target
+                right = mid - 1; // move the right 
+pointer to the left of middle element
+            }
+        }
+        
+        return -1; // target not found in the array
+    }
+}
