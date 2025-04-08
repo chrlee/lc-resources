99f1c3415d68af6bb824c86e2864e2df2f7adedf
chrlee <christianjslee@gmail.com>
2025-04-08 12:59:54 -0400

    Solve: binary-search. Binary Search (java)

---
+++ leetcode/binary-search_binary_search_1744131592261.java
@@ -0,0 +1,12 @@
+calculate the middle index of the array
+            int mid = left + (right - left) / 2; // 
+right pointer
+loop till left pointer is less than or equal to 
+        while (left <= right) { // continue the 
+        
+right pointer to the last index of the array
+        int right = nums.length - 1; // initialize 
+0
+        int left = 0; // initialize left pointer to 
+    public int search(int[] nums, int target) {
+class Solution {
