17acf4891ede53577ba991ea29a0a824d14a4776
chrlee <christianjslee@gmail.com>
2025-06-09 16:19:42 -0400

    Solution for 3sum: 3Sum (typescript)

---
+++ leetcode-solutions/3sum_3sum/solutions/solution_1749500381695.ts
@@ -0,0 +1,12 @@
+                const output = [sorted[i], sorted[j], (sorted[i] + sorted[j]) * 
+            }
+                const key = output.join(',');
+                out.set(key, output);
+(-1)];
+            if(map.has((sorted[i] + sorted[j])*(-1))){
+        const map = new Map<number, number>();
+        for(let j = i+1; j < sorted.length; ++j){
+    const sorted = nums.toSorted();
+    for(let i = 0; i < sorted.length; ++i){
+function threeSum(nums: number[]): number[][] {
+    const out = new Map<string, number[]>();
