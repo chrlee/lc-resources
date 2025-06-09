d93d49ce2318dd5bec54aa0c63027f9ce90ec2eb
chrlee <christianjslee@gmail.com>
2025-06-09 16:33:12 -0400

    Solution for two-sum-ii-input-array-is-sorted: Two Sum II - Input Array Is Sorted (typescript)

---
+++ leetcode-solutions/two-sum-ii-input-array-is-sorted_two_sum_ii_-_input_array_is_sorted/solutions/solution_1749501192404.ts
@@ -0,0 +1,12 @@
+    }
+            return [l+1, r+1];
+        } else if(sum > target){
+            r--;
+        } else {
+            l++;
+        }
+        if(sum === target){
+        const sum = numbers[l] + numbers[r];
+    while(l < r){
+    let [l,r] = [0,numbers.length-1];
+function twoSum(numbers: number[], target: number): number[] {
