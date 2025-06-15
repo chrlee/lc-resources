2ec5eb318766c724c0fd81bee8a2f6c2d5ea17a6
chrlee <christianjslee@gmail.com>
2025-06-15 15:31:06 -0400

    Solution for longest-substring-without-repeating-characters: Longest Substring Without Repeating Characters (typescript)

---
+++ leetcode-solutions/longest-substring-without-repeating-characters_longest_substring_without_repeating_characters/solutions/solution_1750015866071.ts
@@ -0,0 +1,17 @@
+        if(!set.has(s[r])){
+            set.add(s[r]);
+        } else {
+            while(s[l] !== s[r]){
+        }
+                l++;
+            }
+                set.delete(s[l]);
+            l++;
+        r++;
+    set.add(s[0]);
+    while(r < s.length){
+    let [l,r] = [0,1];
+    let maxLength = 0;
+    const set = new Set<string>();
+    if(s.length < 2) return s.length;
+function lengthOfLongestSubstring(s: string): number {
