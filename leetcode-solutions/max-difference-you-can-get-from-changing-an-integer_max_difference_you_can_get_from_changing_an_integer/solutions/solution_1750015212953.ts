04e98b4efc339c2b63d7f2667d0435eb2766a6fe
chrlee <christianjslee@gmail.com>
2025-06-15 15:20:13 -0400

    Solution for max-difference-you-can-get-from-changing-an-integer: Max Difference You Can Get From Changing an Integer (typescript)

---
+++ leetcode-solutions/max-difference-you-can-get-from-changing-an-integer_max_difference_you_can_get_from_changing_an_integer/solutions/solution_1750015212953.ts
@@ -0,0 +1,12 @@
+function maxDiff(num: number): number {
+    const str = num.toString().split('');
+    const aStartIdx = str.findIndex((c) => c !== '9');
+    const singleDigit = str.every((c) => c === str[0]);
+    const bStartIdx = str.findIndex((c, i) => c !== '1' && c !=='0');
+
+    const a = +((aStartIdx > -1 ? str.map((c) => c === str[aStartIdx] ? '9' : c) : str).join(''));
+    const b = +((bStartIdx > -1 ? str.map((c) => c === str[bStartIdx] ? `${singleDigit || bStartIdx === 0 ? '1' : '0'}
+` : c) : str).join(''));
+
+    return a-b;
+};
